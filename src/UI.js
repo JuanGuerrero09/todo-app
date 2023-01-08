80c4b64219e1a7e0d151b515dfc2981ccdd1a077
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-01-08 14:37:25 -0500

    little changes in html creation

--- src/UI.js
+++ src/UI.js
@@ -8,15 +8,14 @@ class UI {
         activeTasks.forEach(ToDo => {
             const taskEl = document.createElement('div')
             taskEl.classList.add('task-el')
-            console.log(ToDo)
             taskEl.innerHTML = `
             <input type="checkbox" name="" id="">
             <h2>${ToDo.title}</h2>
             <p>${ToDo.description}</p>
-            <h4>${ToDo.priority}</h4>
+            <h4>${ToDo.priority} Priority</h4>
             <h6>${ToDo.dueDate}</h6>
             <div class="el-btns">
-                <a><i class="fa-solid fa-pen-to-square edit-btn"></i></a>
+                <a><i class="fa-solid fa-pen-to-square edit-btn" id="${ToDo.id}"></i></a>
                 <a><i class="fa-solid fa-trash trash-btn" id="${ToDo.id}"></i></a>
             </div>            
             `
